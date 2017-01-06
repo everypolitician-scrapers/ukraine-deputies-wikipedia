54df25fcfc06b2ce9ea4df62bfdcc69c59cac8eb
Tony Bowden <tony@tmtm.com>
2017-01-06 13:11:59 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -10,7 +11,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -29,36 +30,35 @@ def scrape_list(url)
 
   noko.xpath('//table[contains(caption,"Народні депутати VIII скликання")]//tr[td]').each do |tr|
     tds = tr.css('td')
-    data = { 
-      name: tds[2].text,
-      wikiname: tds[2].xpath('.//a[not(@class="new")]/@title').text,
-      party: tds[1].text.tidy,
+    data = {
+      name:        tds[2].text,
+      wikiname:    tds[2].xpath('.//a[not(@class="new")]/@title').text,
+      party:       tds[1].text.tidy,
       list_number: tds[3].text.tidy,
-      area_id: tds[4].text.tidy,
-      faction: tds[5].text.tidy,
-      term: 8,
-      start_date: date_from(tds[6].text)
+      area_id:     tds[4].text.tidy,
+      faction:     tds[5].text.tidy,
+      term:        8,
+      start_date:  date_from(tds[6].text),
     }
     # puts data
     # Let's assume there aren't two people in the same party with the same name
-    ScraperWiki.save_sqlite([:name, :party, :term], data)
+    ScraperWiki.save_sqlite(%i(name party term), data)
   end
 
   noko.xpath('//h3[contains(span,"Депутати, що вибули")]/following-sibling::table[1]/tr[td]').each do |tr|
     tds = tr.css('td')
-    data = { 
-      name: tds[2].text,
-      wikiname: tds[2].xpath('.//a[not(@class="new")]/@title').text,
-      party: tds[1].text.tidy,
+    data = {
+      name:        tds[2].text,
+      wikiname:    tds[2].xpath('.//a[not(@class="new")]/@title').text,
+      party:       tds[1].text.tidy,
       list_number: tds[3].text.tidy,
-      area_id: tds[4].text.tidy,
-      term: 8,
-      end_date: date_from(tds[5].text)
+      area_id:     tds[4].text.tidy,
+      term:        8,
+      end_date:    date_from(tds[5].text),
     }
-    # puts data
-    ScraperWiki.save_sqlite([:name, :party, :term], data)
+    # puts data
+    ScraperWiki.save_sqlite(%i(name party term), data)
   end
-
 end
 
 scrape_list('https://uk.wikipedia.org/wiki/%D0%92%D0%B5%D1%80%D1%85%D0%BE%D0%B2%D0%BD%D0%B0_%D0%A0%D0%B0%D0%B4%D0%B0_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B8_VIII_%D1%81%D0%BA%D0%BB%D0%B8%D0%BA%D0%B0%D0%BD%D0%BD%D1%8F')
